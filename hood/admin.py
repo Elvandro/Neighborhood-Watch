408f0b0853b2e15f58f4e7c47543b522e9f67e98
Elvandro <elvisamuni22@gmail.com>
2018-08-07 12:06:07 +0300

    Setup the admin file

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Profile, Neighbourhood, Join, Business, Post, Comment
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Neighbourhood)
+admin.site.register(Join)
+admin.site.register(Business)
+admin.site.register(Post)
+admin.site.register(Comment)
